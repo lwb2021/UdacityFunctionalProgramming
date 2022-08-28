c5aabbde475d878293251b384c209481e7e1cedd
Barry Ke <lbke2008@gmail.com>
2022-08-28 16:41:29 +0800

    lesson 04

--- fp_in_js/render_method_exercise/render_method.js
+++ fp_in_js/render_method_exercise/render_method.js
@@ -1,35 +1,54 @@
 // Here is the code from the example text
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
 const Welcome = () => {
-    return `Welcome to my Javascript Program!`
-}
+  return `Welcome to my Javascript Program!`;
+};
 
 const App = () => {
-    return `
+  return `
         <h1>${Welcome()}<h1>
         <div> I EXIST! </div>
-    `
-}
+    `;
+};
 
-const render = root => {
-    root.innerHTML = App()
-}
+const render = (root) => {
+  //   root.innerHTML = App();
+  root.innerHTML = menu();
+};
 
-render(root)
+render(root);
 
 // Add a new Menu component that takes in a show argument which is either true or false
 // Show this content if show is true:
-{/* <nav>
+{
+  /* <nav>
     <ul>
         <li>About Us</li>
         <li>Contact Us</li>
         <li>Login</li>
     </ul>
-</nav> */}
+</nav> */
+}
 
 // and this content if show is false:
-{/* <nav>Menu</nav> */ }
+{
+  /* <nav>Menu</nav> */
+}
 
 // Your Code Here
+
+const menu = (show = true) => {
+  if (show) {
+    return `<nav>
+                <ul>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Login</li>
+                </ul>
+            </nav>`;
+  } else {
+    `<nav>Menu</nav>`;
+  }
+};
