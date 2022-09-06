15b9b35e2e17b5e8ad8fbe542bda3cacec28bcbb
lwb2021 <64982612+lwb2021@users.noreply.github.com>
2022-09-06 11:57:00 +0800

    Intermediate JavaScript Nanodegree Project 2 Mars Dashboard #2 (#2)
    * init commit
    
    * get familiar with the project
    
    * add cascading dropdown menus
    
    * deliver the 1st MVP;
    
    * modify the structure of client.js and change
    the display;
    
    * deliver the final version of the project
    
    * - change let to const
    - fix the use of immutable.js
    - open a new link when clicking the link of a picture;
    - fix the UI issue on mobile
    - fix page title

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,105 +1,210 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+const opportunityCameraList = [
+  "Front Hazard Avoidance Camera",
+  "Rear Hazard Avoidance Camera",
+  "Navigation Camera",
+  "Panoramic Camera",
+  "Miniature Thermal Emission Spectrometer (Mini-TES)",
+];
 
-// add our markup to the page
-const root = document.getElementById('root')
+const curiosityCameraList = [
+  "Front Hazard Avoidance Camera",
+  "Rear Hazard Avoidance Camera",
+  "Navigation Camera",
+  "Mast Camera",
+  "Chemistry and Camera Complex",
+  "Mars Hand Lens Imager",
+  "Mars Descent Imager",
+];
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+let store = Immutable.Map({
+  apod: "",
+  photosArr: Immutable.List([]),
+  firstLoad: true,
+  rovers: Immutable.List(["Curiosity", "Opportunity", "Spirit"]),
+  cameras: Immutable.Map({
+    Curiosity: curiosityCameraList,
+    Opportunity: opportunityCameraList,
+    Spirit: opportunityCameraList, // Spirit has the same cameras as Opportunity
+  }),
+});
 
-const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
+const root = document.getElementById("root");
+const controlPanel = document.getElementById("controlPanel");
 
+const updateStore = (state, newState) => {
+  store = state.merge(newState);
+  render(root, store);
+};
+
+const render = (root, store) => {
+  root.innerHTML = App(store);
+};
+
+const generateInfoTable = (store) => {
+  const table = document.getElementById("table");
+  const { rover, camera, img_src } = store.get("photosArr").get(0);
+  const tableHTML = `
+    <tr>
+      <td>Rover</td>
+      <td>${rover.name}</td>
+    </tr>
+    <tr>
+      <td>Camera</td>
+      <td>${camera.full_name}</td>
+    </tr>
+    <tr>
+      <td>Launch Date</td>
+      <td>${rover.launch_date}</td>
+    </tr>
+    <tr>
+      <td>Landing Date</td>
+      <td>${rover.landing_date}</td>
+    </tr>
+    <tr>
+      <td>Status</td>
+      <td>${rover.status}</td>
+    </tr>
+    <tr>
+      <td>Most Recently Available Photo</td>
+      <td><a target=”_blank” href="${img_src}">link</a></td>
+    </tr>
+  `;
+  table.style.display = "flex";
+  table.innerHTML = tableHTML;
+};
+
+const getControlPanel = () => {
+  return `            
+          <form id="form"> 
+            <div class="form-group">
+              <label>Rover</label>
+              <select id="rover" name="rover" class="form-select form-select-style">
+                    ${store
+                      .get("rovers")
+                      .toArray()
+                      .map((rover) => `<option>${rover}</option>`)
+                      .join("")}
+              </select> 
+            </div>
+
+            <div class="form-group">
+              <label>Camera</label>
+              <select id="camera" name="camera" class="form-select form-select-style">
+                    ${store
+                      .get("cameras")
+                      .get(store.get("rovers").get(0))
+                      .map((rover) => `<option>${rover}</option>`)
+                      .join("")}
+              </select>
+            </div>
+            <button type="submit">Submit</button> 
+          </form>
+        `;
+};
 
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
-
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
-}
+  if (state.get("photosArr").toArray().length != 0) generateInfoTable(store);
+  return `
+    ${
+      state.get("photosArr").toArray().length == 0
+        ? displayBlank()
+        : displayPhotos(state.get("photosArr").toArray())
+    }
+  `;
+};
 
 // listening for load event because page should load before any JS is called
-window.addEventListener('load', () => {
-    render(root, store)
-})
+window.addEventListener("load", () => {
+  controlPanel.innerHTML = getControlPanel();
+  render(root, store);
 
-// ------------------------------------------------------  COMPONENTS
+  const roverSelect = document.getElementById("rover");
+  const cameraSelect = document.getElementById("camera");
+  const form = document.getElementById("form");
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
+  roverSelect.addEventListener("change", () => {
+    cameraSelect.innerHTML = store
+      .get("cameras")
+      .get(roverSelect.value)
+      .map((camera) => `<option>${camera}</option>`);
+  });
 
-    return `
-        <h1>Hello!</h1>
-    `
-}
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    getRoverPhoto(roverSelect.value, cameraSelect.value);
+  });
+});
 
-// Example of a pure function that renders infomation requested from the backend
 const ImageOfTheDay = (apod) => {
+  // If image does not already exist, or it is not from today -- request it again
+  const today = new Date();
+  if (!apod || apod.date === today.getDate()) {
+    getImageOfTheDay(store);
+  }
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
+  // check if the photo of the day is actually type video!
+  if (apod.image && apod.image.media_type === "video") {
+    return `
+            <p>See today's featured video <a href="${apod.image.url}">here</a></p>
+            <p>${apod.image.title}</p>
             <p>${apod.image.explanation}</p>
-        `)
-    }
-}
+        `;
+  } else if (apod.image && apod.image.media_type === "image") {
+    return `
+            <img src="${apod.image.url}" />
+            <div>
+              <h1 class="header">Astronomy Picture of the Day</h1>
+              <p>${apod.image.explanation}</p>
+            </div>
+        `;
+  } else {
+    return `<h1>Loading...</h1>`;
+  }
+};
 
-// ------------------------------------------------------  API CALLS
+const displayBlank = () => {
+  if (store.get("firstLoad")) {
+    return ImageOfTheDay(store.get("apod"));
+  } else {
+    const table = document.getElementById("table");
+    table.style.display = "none";
+    return "<h1>Oops, no photo was found for this choice.</h1>";
+  }
+};
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+const displayPhotos = (photosArr) => {
+  return photosArr
+    .map((photo) => `<img src="${photo.img_src}"></img>`)
+    .join("");
+};
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+const getImageOfTheDay = () => {
+  fetch(`http://localhost:3000/apod`)
+    .then((res) => res.json())
+    .then((apod) => updateStore(store, { apod }));
+};
 
-    return data
-}
+const getCameraAbbrev = (camera) => {
+  const lookup = Immutable.Map({
+    "Front Hazard Avoidance Camera": "FHAZ",
+    "Rear Hazard Avoidance Camera": "RHAZ",
+    "Mast Camera": "MAST",
+    "Chemistry and Camera Complex": "CHEMCAM",
+    "Mars Hand Lens Imager": "MAHLI",
+    "Mars Descent Imager": "MARDI",
+    "Navigation Camera": "NAVCAM",
+    "Panoramic Camera": "PANCAM",
+    "Miniature Thermal Emission Spectrometer (Mini-TES)": "MINITES",
+  });
+  return lookup.get(camera);
+};
+
+const getRoverPhoto = (rover, camera) => {
+  fetch(`http://localhost:3000/mrp/${rover}/${getCameraAbbrev(camera)}`)
+    .then((res) => res.json())
+    .then((photos) => {
+      const photosArr = Immutable.List(photos.image.photos);
+      updateStore(store, Immutable.Map({ photosArr, firstLoad: false }));
+    });
+};
